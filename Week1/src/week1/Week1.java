d0c5e1e4c251660ba1dd55848ccefc61fcbc1c07
Asus <Asus@LAPTOP-5LGE75P4>
2021-07-24 16:42:48 +0530

    Committing Week1 project

---
+++ Week1/src/week1/Week1.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package week1;
+
+/**
+ * @author Anil Kumar C0818101
+ * Sample program
+ */
+public class Week1 {
+
+	/**
+	 * @param args
+	 * Print a name
+	 */
+	public static void main(String[] args) {
+		// Output
+		System.out.println("My name is Anil Kumar B");
+
+	}
+
+}
